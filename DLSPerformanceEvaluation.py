5393bf1017e7adc35189d5211b4900461809a691
KPatWSU <kefir.pelly@wsu.edu>
2025-11-14 21:26:42 -0800

    "Performance-evaluation" function first draft

---
+++ DLSPerformanceEvaluation.py
@@ -0,0 +1,15 @@
+from WeightedGraph import *
+import math
+import numpy
+
+def evaluate(pointArray, neighborsGraph, threshold):
+    successCount = 0
+    failCount = 0
+    for i in range(len(pointArray) - 1):
+        for j in range(i + 1, len(pointArray)):
+            if math.sqrt(sum(numpy.square(numpy.subtract(pointArray[i], pointArray[j])))) <= threshold:
+                if neighborsGraph.adj_matrix[i][j]: #works off of truthiness
+                   successCount += 1
+                else:
+                    failCount += 1
+    return (successCount, failCount)
